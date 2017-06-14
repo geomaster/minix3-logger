34e1ffc5b4652a029d235a4163007a033fe8fe85
David Davidović <david@davidovic.io>
2017-06-14 12:18:40 +0200

    Add constants and build `ls` with the source
    Added `ls` to necessary tables in `rs` and `kernel`.
    
    Added constants relating to `ls`'s PID and available IPC message type
    numbers to `com.h`.
    
    Added a definition of the `ls` server to `/etc/system.conf`.
    
    Added an entry to the `servers` Makefile to build `ls` alongside the
    other servers.

--- usr/src/minix/kernel/table.c
+++ usr/src/minix/kernel/table.c
@@ -61,6 +61,7 @@ struct boot_image image[NR_BOOT_PROCS] = {
 {MFS_PROC_NR,   "mfs"   },
 {VM_PROC_NR,    "vm"    },
 {PFS_PROC_NR,   "pfs"   },
+{LS_PROC_NR,    "ls"    },
 {INIT_PROC_NR,  "init"  },
 };
 
